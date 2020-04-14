7e1510812f0c5a9fb68eab5720b4720ad49aa05d
Fabian Beyerlein <dev@beyerleinf.de>
2020-04-14 23:48:36 +0200

    feat: Add i18n support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { Library, TranslateObjectLoader } from 'translate-object-loader';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './shared/components/app/app.component';
-import { LoginComponent } from './shared/components/login/login.component';
-import { AuthRouteHostComponent } from './shared/components/auth-route-host/auth-route-host.component';
 import { SharedMaterialModule } from './shared-material.module';
+import { AppComponent } from './shared/components/app/app.component';
+import { AuthRouteHostComponent } from './shared/components/auth-route-host/auth-route-host.component';
+import { LoginComponent } from './shared/components/login/login.component';
+import { sharedTranslations } from './shared/shared-translations';
+
+Library.add(sharedTranslations);
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, AuthRouteHostComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, SharedMaterialModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedMaterialModule,
+    TranslateModule.forRoot({
+      loader: { provide: TranslateLoader, useClass: TranslateObjectLoader },
+      useDefaultLang: true,
+      defaultLanguage: 'de',
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/shared/shared-translations.ts
@@ -0,0 +1,18 @@
+import { TranslationDefinition } from 'translate-object-loader';
+
+export const sharedTranslations: TranslationDefinition = {
+  de: {
+    login: {
+      login: 'Login',
+      username: 'Benutzername',
+      password: 'Passwort',
+    },
+  },
+  en: {
+    login: {
+      login: 'Login',
+      username: 'Username',
+      password: 'Password',
+    },
+  },
+};
